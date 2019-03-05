db2a00daaf78085907076b8a165e8c447c2ecb77
js665999 <jakub.sobek@broadcom.com>
2019-03-05 14:29:12 +0100

    tests

--- packages/imperative/__tests__/Imperative.test.ts
+++ packages/imperative/__tests__/Imperative.test.ts
@@ -141,46 +141,15 @@ describe("Imperative", () => {
         });
 
         describe("AppSettings", () => {
+            const defaultSettings =  {overrides: {CredentialManager: false}};
             it("should initialize an app settings instance", async () => {
                 await Imperative.init();
 
                 expect(mocks.AppSettings.initialize).toHaveBeenCalledTimes(1);
                 expect(mocks.AppSettings.initialize).toHaveBeenCalledWith(
                     join(mocks.ImperativeConfig.instance.cliHome, "settings", "imperative.json"),
-                    expect.any(Function)
+                    defaultSettings
                 );
-            });
-
-            it("should create settings.json if it is missing", async () => {
-                await Imperative.init();
-
-                expect(mocks.AppSettings.initialize).toHaveBeenCalledTimes(1);
-
-                // Mimic us executing the callback
-                jest.doMock("../../io");
-                jest.doMock("jsonfile");
-
-                const {IO} = require("../../io");
-                const {writeFileSync} = require("jsonfile");
-
-                const settingsFile = generateRandomAlphaNumericString(16, true); // tslint:disable-line
-                const defaultSetttings = {
-                    test: generateRandomAlphaNumericString(16, true) // tslint:disable-line
-                };
-
-                const returnVal = mocks.AppSettings.initialize.mock.calls[0][1](settingsFile, defaultSetttings);
-
-                expect(IO.createDirsSyncFromFilePath).toHaveBeenCalledTimes(1);
-                expect(IO.createDirsSyncFromFilePath).toHaveBeenCalledWith(settingsFile);
-
-                expect(writeFileSync).toHaveBeenCalledTimes(1);
-                expect(writeFileSync).toHaveBeenCalledWith(settingsFile, defaultSetttings, expect.any(Object));
-
-                expect(returnVal).toBe(defaultSetttings);
-
-
-                jest.dontMock("../../io");
-                jest.dontMock("jsonfile");
             });
         }); // End AppSettings
 
